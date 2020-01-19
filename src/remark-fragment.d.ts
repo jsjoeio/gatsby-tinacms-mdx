866dbba46b466ae07a5d44e331826a72f66ddb36
JavaScript Joe <jjprevite@gmail.com>
2020-01-19 13:27:10 -0700

    fix: add deps to get package to build

--- src/remark-fragment.d.ts
+++ src/remark-fragment.d.ts
@@ -15,4 +15,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */
-export declare const tinaRemarkFragment: void;
+export declare const tinaRemarkFragment: any;
